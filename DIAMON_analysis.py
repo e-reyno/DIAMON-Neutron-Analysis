f97a6d324c88b1a5c2eab2f4ae2bd4a9ad84c720
e-reyno <110035645+e-reyno@users.noreply.github.com>
2022-10-05 16:22:26 +0100

    added peak finder

--- DIAMON_analysis.py
+++ DIAMON_analysis.py
@@ -16,7 +16,7 @@ def plot_spect(data):
     plt.legend()
     plt.savefig("single_energy_spectrum.png")
     plt.show()
-    
+
 
 def plot_combined_spect(data_array):
     
@@ -27,10 +27,14 @@ def plot_combined_spect(data_array):
         plt.step(energy, flux, label=data.name)
         plt.xlabel("Energy (MeV)")
         plt.ylabel("Flux (cm$^{-2}$s$^{-1}$)")
-        plt.legend()
+        plt.legend(fontsize=12, loc=1)
     plt.savefig("combined_energy_spectra.png")
     plt.show()
     
+
+#def contour_plot()
+
+
 
 def plot_detector_counts(rate_data, labels):
     for i, rate in enumerate(rate_data):
@@ -46,10 +50,12 @@ def plot_detector_counts(rate_data, labels):
     
 def plot_dose_rate(df):
     
+    plt.figure(figsize=(16,16))
     ax = df["dose_rate"].plot(kind='bar', yerr=(df["dose_rate_uncert"])/100 * df['dose_rate'], capsize=4, color='purple')
-    ax.set_xlabel('date')
+    ax.set_xlabel('Date of Measurements')
     ax.set_ylabel('dose rate ($\mu$ Sv\h)')
-    ax.set_xticklabels(df["name"], rotation=45)
+    ax.set_xticklabels(df["name"], rotation=20)
+
     plt.savefig("dose_rate_bar_plot.png")
     plt.show()
     
@@ -129,13 +135,22 @@ def find_abs_error(dataframe):
 
 def stack_bar_plot(data_frame, xlabel=None, ylabel=None):
     #stack_df = (data_frame.filter(cols)).astype(float)
-    ax = data_frame[["thermal", "epi", "fast"]].plot(kind='bar', stacked=True, figsize = (12,8))
+
+    ax = data_frame[["thermal", "epi", "fast"]].plot(kind='bar', stacked=True,figsize=(20,20))
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
-    ax.set_xticklabels(data_frame["name"])
+    ax.set_xticklabels(data_frame["name"], rotation = 45)
     plt.savefig("energy_ranges_bar_plot.png")
     plt.show()
+ 
+def direction_bar_plot(dataframe):
     
+    ax = dataframe[["F", "FL", "FR", "R", "RR", "RL"]].plot(kind='bar',figsize = (12,8))
+    ax.set_ylabel("Counts")
+    ax.set_xticklabels(dataframe["name"], rotation=60)
+    plt.show()
+
+   
 def find_spect_peaks(data):
     
     energy_list = []
@@ -164,9 +179,13 @@ folder_path = r"C:\Users\sfs81547\Documents\DIAMON project\TS1 measurements\*"
 #reads in the folder contianing data for: unfold, rate and out data separated into columns of the measurement
 all_data = dia.read_folder(folder_path)
 all_data = np.array(all_data)
+all_data = all_data[1:,:]
 
 
 energy_peaks, flux_peaks = find_spect_peaks(all_data[:,0])
+df = pd.DataFrame({'energy peaks' :energy_peaks, 'flux peaks' : flux_peaks}, columns=['energy peaks', 'flux peaks'])
+df1 = df.explode('energy peaks', "flux peaks")
+df1.to_csv("energy_flux_peaks1.csv")
 print(energy_peaks)
 print(flux_peaks)
 avg_flux = average_daily_data(all_data[:,0])
@@ -177,11 +196,13 @@ unfold_datalist = []
 for data in all_data[:,0]:
     unfold_data = dia.convert_to_ds(data)
     unfold_datalist.append(unfold_data)
+
+plot_combined_spect(all_data[:,0])
     
 unfold_dataframe = pd.DataFrame(unfold_datalist)
+direction_bar_plot(unfold_dataframe)
 stack_bar_plot(unfold_dataframe)
 plot_dose_rate(unfold_dataframe)
-plot_combined_spect(all_data[:,0])
 names = [data[0].name for data in all_data]
 plot_detector_counts(all_data[:,2], names)
 
